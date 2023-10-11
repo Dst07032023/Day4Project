aac79079b5d08da7f8c7dd9c8a58c12acf1796f0
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-11 20:18:57 +0200

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,5 +2,11 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        //create new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
     }
 }
