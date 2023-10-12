1935a7fdef6ce200cb2fe7abe97c8e21230e1b2e
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-12 09:42:19 +0200

    work2

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -6,7 +6,7 @@ public class BranchingPractice {
         //enabled version control
         //added .gitignore with .idea, target folder and .iml file
         //made initial commit by keeping track of all files
-        //shared project on github
+        //shared project on GitHub
 
         //reviewing on new branch
         //doing more review on branch
